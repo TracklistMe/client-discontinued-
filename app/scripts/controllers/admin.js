daeb1f41a8ec60a1f95825d74101996e4f3fb25e
Nicola Bortignon <info@nicolabortignon.com>
2014-12-16 23:31:53 +0100

    Built tracklistme from commit c52590a on branch master

---
+++ app/scripts/controllers/admin.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tracklistmeApp.controller:AdminCtrl
+ * @description
+ * # AdminCtrl
+ * Controller of the tracklistmeApp
+ */
+angular.module('tracklistmeApp')
+  .controller('AdminCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
