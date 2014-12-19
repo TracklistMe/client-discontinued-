7d776c7fca095acead4d27676f9e7d0789a7f745
Nicola Bortignon <info@nicolabortignon.com>
2014-12-19 23:22:09 +0100

    Start working on Nav Bar

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -8,10 +8,23 @@
  * Controller of the tracklistmeApp
  */
 angular.module('tracklistmeApp')
-  .controller('NavbarCtrl', function ($scope, $auth) {
+  .controller('NavbarCtrl', function ($scope, $auth,Account) {
+
+  
+  	
   	$scope.isAuthenticated = function() {
       return $auth.isAuthenticated();
     };
+    $scope.showHideDropDown = function(){
+    	$scope.showAccountDropDown = !$scope.showAccountDropDown
+    }
+    $scope.showAccountDropDown = true
+    Account.getProfile()
+        .success(function(data) {
+          console.log(data)
+          $scope.user = data;
+        })
+    
 
   
   });
