94c5765df572b24aff39fd5befa301856b82f2d9
Nicola Bortignon <info@nicolabortignon.com>
2014-12-26 16:40:39 +0100

    adding admin panel for hasmany association User-Company

--- app/scripts/controllers/admincompanies.js
+++ app/scripts/controllers/admincompanies.js
@@ -11,16 +11,78 @@ angular.module('tracklistmeApp')
   .controller('AdmincompaniesCtrl', function ($scope,$http) {
     // add new company form
     var CHARACTER_BEFORE_SEARCH = 2;
-    
-    $scope.changed = function() {
-      if($scope.userType.length > CHARACTER_BEFORE_SEARCH){
-      	$http.get('http://localhost:3000/companies/search/'+$scope.userType)
-      	.success(function(data) {
-          console.log(data)
-          $scope.user = data;
-        })
-      }
+    $scope.currentCompany = null;
+    $scope.isSearching = false
+    $scope.nameAvailable = false
+    $scope.nameTooShort = true
+    $scope.searchUserResults = null
+
+    $scope.companyList = {}
+
+    $scope.searchCompaniesAvailability = function() { 
+      	if($scope.searchCompanyField.length > CHARACTER_BEFORE_SEARCH){
+      		$scope.isSearching = true;
+      		$scope.nameTooShort = false;
+      		$http.get('http://localhost:3000/companies/search/'+$scope.searchCompanyField)
+      		.success(function(data) {
+          		
+          		$scope.isSearching = false
+          		if(!data){
+          			//!date --> the object is empty, there is no other company with this name, the name is available
+          			$scope.nameAvailable = true
+          		}else{
+          			//date --> the object has something
+          			$scope.nameAvailable = false
+          		}
+        	})
+      	} else {
+      		$scope.nameTooShort = true;
+      	}
    	};
+   	$scope.addCompany = function(){
+   		console.log("ADD COMPANY")
+   		// TODO STRING SANITIZING
+   		$http.post('http://localhost:3000/companies/', {companyName:$scope.searchCompanyField}).
+		  success(function(data, status, headers, config) {
+		  	$scope.updateCompanyList();
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+   	}
+
+   	$scope.searchUser = function() {
+   		if($scope.searchUserField.length > CHARACTER_BEFORE_SEARCH){
+	   		$http.get('http://localhost:3000/users/search/'+$scope.searchUserField)
+	      		.success(function(data) {
+	          		$scope.searchUserResults = data
+   					console.log($scope.searchUserResults)
+	        	})
+	    }
+   	}
+   	$scope.selectFromMultipleUsers = function(user){
+   		$scope.searchUserResults = [user];
+   		console.log($scope.searchUserResults)
+   	}
+
+    $scope.editCompany = function(idCompany){
+   		$http.get('http://localhost:3000/companies/'+idCompany)
+      		.success(function(data) {
+      				console.log(data)
+          			$scope.currentCompany = data
+          	})
+   	}
+   	$scope.updateCompanyList = function(){
+   		$http.get('http://localhost:3000/companies/')
+      		.success(function(data) {
+      				console.log(data)
+          			$scope.companyList = data
+          		
+        	})
+   	}
+
+   	$scope.updateCompanyList()
 
 
   });
