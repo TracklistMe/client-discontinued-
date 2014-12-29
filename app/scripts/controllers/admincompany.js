5028c5d9c46214a252da39671cded93ef95a9cb4
Nicola Bortignon <info@nicolabortignon.com>
2014-12-29 15:43:40 +0000

    start refactoring the upload function

--- app/scripts/controllers/admincompany.js
+++ app/scripts/controllers/admincompany.js
@@ -10,14 +10,124 @@
 angular.module('tracklistmeApp')
   .controller('AdmincompanyCtrl', function ($scope,$state, $auth, $stateParams,$http,Account, FileUploader) {
   	var companyId = $stateParams.id
-  	$scope.company = {}
+  	var CHARACTER_BEFORE_SEARCH = 4;
+  	var CHARACTER_BEFORE_SEARCH_USER = 3;
+  	$scope.isSearching = false
+    $scope.nameAvailable = false
+    $scope.nameTooShort = true
+    $scope.searchUserResults = null  
+  	$scope.currentLabel = null
+  	$scope.labelList = {}
+  	
 
   	var uploader = $scope.uploader = new FileUploader({
         url: 'http://localhost:3000/upload/',
         headers: {'Authorization': 'Bearer '+$auth.getToken()},
         data: {user: $scope.user},
-
     });
+
+
+  	$scope.addLabel = function(){
+   		console.log("ADD Label")
+   		// TODO STRING SANITIZING
+   		$http.post('http://localhost:3000/labels/', {companyId:$scope.companyId, labelName: $scope.seachLabelField}).
+		  success(function(data, status, headers, config) {
+		  	$scope.updateLabelList();
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+   	}
+
+
+    $scope.searchLabelAvailability = function() { 
+      	if($scope.seachLabelField.length > CHARACTER_BEFORE_SEARCH){
+      		$scope.isSearching = true;
+      		$scope.nameTooShort = false;
+      		$http.get('http://localhost:3000/labels/search/'+$scope.seachLabelField)
+      		.success(function(data) {
+          		
+          		$scope.isSearching = false
+          		if(!data){
+          			//!date --> the object is empty, there is no other company with this name, the name is available
+          			$scope.nameAvailable = true
+          		}else{
+          			//date --> the object has something
+          			$scope.nameAvailable = false
+          		}
+        	})
+      	} else {
+      		$scope.nameTooShort = true;
+      	}
+   	};
+
+   	$scope.addLabelManager = function(){
+   		var newLabelManager = $scope.searchUserResults[0].id;
+   		var labelId = $scope.currentLabel.id;
+   	 
+   	 
+   		$http.post('http://localhost:3000/labels/'+companyId+"/labelManagers/", {newLabelManager:newLabelManager}).
+		  success(function(data, status, headers, config) {
+		  	$scope.editLabel(labelId)
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+   	}
+
+   	$scope.removeLabelManager = function(userId){
+   		var labelId = $scope.currentLabel.id;
+   		$http.delete('http://localhost:3000/labels/'+labelId+"/labelManagers/"+userId).
+		  success(function(data, status, headers, config) {
+		  	$scope.editLabel(labelId)
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+   	}
+
+   	$scope.selectFromMultipleUsers = function(user){
+   		// create as an array to align to api return tyep 
+   		$scope.searchUserResults = [user];
+   		console.log($scope.searchUserResults)
+
+   	}
+
+
+   	$scope.searchUser = function() {
+   		if($scope.searchUserField.length > CHARACTER_BEFORE_SEARCH_USER){
+	   		$http.get('http://localhost:3000/users/search/'+$scope.searchUserField)
+	      		.success(function(data) {
+	          		$scope.searchUserResults = data
+   					console.log($scope.searchUserResults)
+	        	})
+	    }
+   	}
+
+
+
+   	$scope.updateLabelList = function(){
+   		$http.get('http://localhost:3000/companies/'+companyId+"/labels")  // ATTENTION ONLY THE LABEL THAT ARE PART OF THIS COMPANY 
+      		.success(function(data) {
+      				console.log(data)
+          			$scope.labelList = data
+          		
+        	})
+   	}
+
+	$scope.editLabel = function(idLabel){
+   		$http.get('http://localhost:3000/labels/'+idLabel)
+      		.success(function(data) {
+      				console.log(data)
+          			$scope.currentLabel = data
+          	})
+   	}
+
+
+
 
     $scope.getCompany = function(){
  		$http.get('http://localhost:3000/companies/'+companyId)
@@ -26,6 +136,7 @@ angular.module('tracklistmeApp')
         	})
  	}
 
+   	$scope.updateLabelList()
  	$scope.getCompany();
 
 
