8d727a775827c1b861c6fa4f9958da658a5291ae
Nicola Bortignon <info@nicolabortignon.com>
2015-01-03 18:41:13 +0000

    Edit page for Release

--- app/scripts/controllers/adminrelease.js
+++ app/scripts/controllers/adminrelease.js
@@ -9,14 +9,49 @@
  */
 angular.module('tracklistmeApp')
   .controller('AdminreleaseCtrl', function ($scope,$state, $auth, $stateParams,$http,Account, FileUploader) {
-  	$scope.release;
+  	$scope.release = null;
   	$scope.releaseId = $stateParams.id;
+  	$scope.editedTrack = null
+  	$scope.temporaryTrack = null
+  	var CHARACTER_BEFORE_SEARCH = 3
+  	$scope.isSearching = false
+    $scope.nameAvailable = false
+    $scope.nameTooShort = true
+    $scope.searchArtistResults = null
+     
 
 
+  	$scope.searchArtist = function() { 
+      	if($scope.searchArtistField.length > CHARACTER_BEFORE_SEARCH){
+      		$scope.isSearching = true;
+      		$scope.nameTooShort = false;
+      		$http.get('http://localhost:3000/artists/search/'+$scope.searchArtistField)
+      		.success(function(data) {
+      			          		
+          		$scope.isSearching = false
+          		if(!data){
+          			//!date --> the object is empty, there is no other company with this name, the name is available
+          			$scope.nameAvailable = true
+          		}else{
+          			//date --> the object has something
+          			$scope.searchArtistResults = data
+
+          		}
+        	})
+      	} else {
+      		$scope.nameTooShort = true;
+      	}
+   	};
 
 
-
-
+  	$scope.editTrack = function(trackId){
+  		for (var i = $scope.release.Tracks.length - 1; i >= 0; i--) {
+  			if($scope.release.Tracks[i].id == trackId){
+  				$scope.editedTrack = $scope.release.Tracks[i];
+  			}	
+  		};
+  	 
+  	}
 
 
     $scope.getRelease = function(){
