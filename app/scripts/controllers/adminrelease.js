5ca1e14568c0a033af6471d39e0cf1a45b72e40c
Nicola Bortignon <info@nicolabortignon.com>
2015-01-03 19:34:08 +0000

    Add/Remove Producer / Remixer from a track

--- app/scripts/controllers/adminrelease.js
+++ app/scripts/controllers/adminrelease.js
@@ -18,8 +18,52 @@ angular.module('tracklistmeApp')
     $scope.nameAvailable = false
     $scope.nameTooShort = true
     $scope.searchArtistResults = null
-     
+      
 
+
+    $scope.addArtist = function(){
+    	$http.post('http://localhost:3000/artists/', {displayName:$scope.searchArtistField}).
+		  success(function(data, status, headers, config) {
+		  	$scope.searchArtist();
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+    }
+
+    $scope.addAsProducer = function(artist){
+    	$scope.editedTrack.Producer.push(artist);
+
+    }
+    $scope.addAsRemixer = function(artist){
+    	$scope.editedTrack.Remixer.push(artist);
+    }
+
+    $scope.deleteProducer = function(artist){
+    	var index = -1;
+    	for (var i = $scope.editedTrack.Producer.length - 1; i >= 0; i--) {
+    		if($scope.editedTrack.Producer[i].id = artist.id){
+    			index = i;
+    		}	
+    	};
+    	$scope.editedTrack.Producer.splice(index, 1);
+    }
+    $scope.deleteRemixer = function(remixer){
+    	console.log("I'm going to delete")
+    	console.log($scope.editedTrack)
+    	var index = -1;
+    	for (var i = $scope.editedTrack.Remixer.length - 1; i >= 0; i--) {
+    		if($scope.editedTrack.Remixer[i].id = remixer.id){
+    			console.log("Found")
+    			console.log($scope.editedTrack.Remixer[i])
+    			index = i;
+    		}	
+    	};
+    	$scope.editedTrack.Remixer.splice(index, 1);
+    	console.log($scope.editedTrack.Remixer)
+
+    }
 
   	$scope.searchArtist = function() { 
       	if($scope.searchArtistField.length > CHARACTER_BEFORE_SEARCH){
