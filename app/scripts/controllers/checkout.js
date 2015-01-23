a465003eaa85dc7964c1ce42880b069390cbf872
Nicola Bortignon <info@nicolabortignon.com>
2015-01-23 15:28:01 +0000

    Built tracklistme from commit f4103cb on branch master

---
+++ app/scripts/controllers/checkout.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tracklistmeApp.controller:CheckoutCtrl
+ * @description
+ * # CheckoutCtrl
+ * Controller of the tracklistmeApp
+ */
+angular.module('tracklistmeApp')
+  .controller('CheckoutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
