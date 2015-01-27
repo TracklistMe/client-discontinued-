535ca7a62f73f46d7c65d1a4979619b5a5003ab7
Nicola Bortignon <info@nicolabortignon.com>
2015-01-27 18:29:05 +0000

    minor fixes

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,24 +8,31 @@
  * Controller of the tracklistmeApp
  */
 angular.module('tracklistmeApp')
- .controller('ProfileCtrl', function($scope,$http, $timeout, $auth, $alert, Account, FileUploader,CONFIG) {
+    .controller('ProfileCtrl', function($scope, $http, $timeout, $auth, $alert, Account, FileUploader, CONFIG) {
         $scope.labels = {}
         $scope.artists = {}
         $scope.companies = {}
         $scope.serverURL = CONFIG.url;
-        
+
         var uploader = $scope.uploader = new FileUploader({
             url: CONFIG.url + '/upload/profilePicture/500/500',
             method: 'POST',
-            headers: {'Authorization': 'Bearer '+$auth.getToken()},
-            formData:[]
+            headers: {
+                'Authorization': 'Bearer ' + $auth.getToken()
+            },
+            formData: []
         });
         uploader.onBeforeUploadItem = function(item) {
-        item.formData.push({resize:{width:500,height:500}});
+            item.formData.push({
+                resize: {
+                    width: 500,
+                    height: 500
+                }
+            });
         }
-        
 
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
             console.info('onWhenAddingFileFailed', item, filter, options);
         };
         uploader.onAfterAddingFile = function(fileItem) {
@@ -36,7 +43,7 @@ angular.module('tracklistmeApp')
         uploader.onAfterAddingAll = function(addedFileItems) {
             console.info('onAfterAddingAll', addedFileItems);
         };
-         
+
 
         uploader.onProgressItem = function(fileItem, progress) {
             console.info('onProgressItem', fileItem, progress);
@@ -61,122 +68,161 @@ angular.module('tracklistmeApp')
             $scope.getProfile();
         };
 
- 
-    /**
-     * Get user's profile information.
-     */
-    $scope.getProfile = function() {
 
-      Account.getProfile()
-        .success(function(data) {
-          $scope.user = data;
-          $scope.user.avatar = CONFIG.url+"/images/"+data.avatar;
-          console.log($scope.user)
-        })
-        .error(function(error) {
-          $alert({
-            content: error.message,
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        });
-    };
+        /**
+         * Get user's profile information.
+         */
+        $scope.getProfile = function() {
 
-
-    /**
-     * Update user's profile information.
-     */
-    $scope.updateProfile = function() {
-      Account.updateProfile({
-        displayName: $scope.user.displayName,
-        email: $scope.user.email
-      }).then(function() {
-        $alert({
-          content: 'Profile has been updated',
-          animation: 'fadeZoomFadeDown',
-          type: 'material',
-          duration: 3
-        });
-      });
-    };
-
-    /**
-     * Link third-party provider.
-     */
-    $scope.link = function(provider) {
-      $auth.link(provider)
-        .then(function() {
-          $alert({
-            content: 'You have successfully linked ' + provider + ' account',
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        })
-        .then(function() {
-          $scope.getProfile();
-        })
-        .catch(function(response) {
-          $alert({
-            content: response.data.message,
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        });
-    };
-
-    /**
-     * Unlink third-party provider.
-     */
-    $scope.unlink = function(provider) {
-      $auth.unlink(provider)
-        .then(function() {
-          $alert({
-            content: 'You have successfully unlinked ' + provider + ' account',
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        })
-        .then(function() {
-          $scope.getProfile();
-        })
-        .catch(function(response) {
-          $alert({
-            content: response.data ? response.data.message : 'Could not unlink ' + provider + ' account',
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        });
-    };
-
- 
- 	$scope.getCompanies = function(){
- 		$http.get(CONFIG.url + '/me/companies/')
-      		.success(function(data) {
-          		$scope.companies = data
-        	})
- 	}
-  $scope.getLabels = function(){
-    $http.get(CONFIG.url + '/me/labels/')
-          .success(function(data) {
-              $scope.labels = data
-          })
-  }
-  $scope.getArtists = function(){
-    $http.get(CONFIG.url + '/me/artists/')
-          .success(function(data) {
-              $scope.artists = data
-          })
-  }
-
-  $scope.getArtists();
-  $scope.getProfile();
-  $scope.getLabels();
- 	$scope.getCompanies();
+            Account.getProfile()
+                .success(function(data) {
+                    $scope.user = data;
+                    $scope.user.avatar = CONFIG.url + "/images/" + data.avatar;
+                    console.log($scope.user)
+                })
+                .error(function(error) {
+                    $alert({
+                        content: error.message,
+                        animation: 'fadeZoomFadeDown',
+                        type: 'material',
+                        duration: 3
+                    });
+                });
+        };
 
 
-  });
+        /**
+         * Update user's profile information.
+         */
+        $scope.updateProfile = function() {
+            Account.updateProfile({
+                displayName: $scope.user.displayName,
+                email: $scope.user.email
+            }).then(function() {
+                $alert({
+                    content: 'Profile has been updated',
+                    animation: 'fadeZoomFadeDown',
+                    type: 'material',
+                    duration: 3
+                });
+            });
+        };
+
+        /**
+         * Link third-party provider.
+         */
+        $scope.link = function(provider) {
+            $auth.link(provider)
+                .then(function() {
+                    $alert({
+                        content: 'You have successfully linked ' + provider + ' account',
+                        animation: 'fadeZoomFadeDown',
+                        type: 'material',
+                        duration: 3
+                    });
+                })
+                .then(function() {
+                    $scope.getProfile();
+                })
+                .catch(function(response) {
+                    $alert({
+                        content: response.data.message,
+                        animation: 'fadeZoomFadeDown',
+                        type: 'material',
+                        duration: 3
+                    });
+                });
+        };
+
+        /**
+         * Unlink third-party provider.
+         */
+        $scope.unlink = function(provider) {
+            $auth.unlink(provider)
+                .then(function() {
+                    $alert({
+                        content: 'You have successfully unlinked ' + provider + ' account',
+                        animation: 'fadeZoomFadeDown',
+                        type: 'material',
+                        duration: 3
+                    });
+                })
+                .then(function() {
+                    $scope.getProfile();
+                })
+                .catch(function(response) {
+                    $alert({
+                        content: response.data ? response.data.message : 'Could not unlink ' + provider + ' account',
+                        animation: 'fadeZoomFadeDown',
+                        type: 'material',
+                        duration: 3
+                    });
+                });
+        };
+
+
+        $scope.getCompanies = function() {
+            $http.get(CONFIG.url + '/me/companies/')
+                .success(function(data) {
+                    $scope.companies = data
+                })
+        }
+        $scope.getLabels = function() {
+            $http.get(CONFIG.url + '/me/labels/')
+                .success(function(data) {
+                    $scope.labels = data
+                })
+        }
+        $scope.getArtists = function() {
+            $http.get(CONFIG.url + '/me/artists/')
+                .success(function(data) {
+                    $scope.artists = data
+                })
+        }
+        $scope.importArtistFromBeatport = function() {
+            $http.get('http://tracklist.me/API/parseArtists.php').success(function(data) {
+
+                for (var i = data.length - 1; i >= 0; i--) {
+                    console.log(data[i]);
+
+                    $scope.importImage(data[i].name, data[i].image)
+                };
+
+            }).error(function(data, status, headers, config) {
+                console.log(data)
+            });
+        }
+
+        $scope.importImage = function(name, image) {
+            var DOM_img = document.createElement("img");
+            //DOM_img.src = image;
+            //document.body.appendChild(DOM_img);
+
+            $http.post(CONFIG.url + '/artists/', {
+                displayName: name
+            }).
+            success(function(data, status, headers, config) {
+
+
+
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
+
+
+
+
+        }
+
+
+
+        $scope.getArtists();
+        $scope.getProfile();
+        $scope.getLabels();
+        $scope.getCompanies();
+
+
+    });
