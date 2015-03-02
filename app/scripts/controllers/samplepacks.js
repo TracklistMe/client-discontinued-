7bc2779aed7805038f09dee36596323d2571725a
Nicola Bortignon <info@nicolabortignon.com>
2015-03-02 13:03:05 +0000

    packages controller

---
+++ app/scripts/controllers/samplepacks.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tracklistmeApp.controller:SamplepacksCtrl
+ * @description
+ * # SamplepacksCtrl
+ * Controller of the tracklistmeApp
+ */
+angular.module('tracklistmeApp')
+  .controller('SamplepacksCtrl', function ($scope, $http, CONFIG) {
+  			$scope.value = null;
+  			$scope.currency = "";
+  			$scope.idproduct = ""
+
+		   var handler = StripeCheckout.configure({
+	              name: "Custom Example",
+	              token: function(token, args) {
+	               
+	              }
+	          });
+
+		    $scope.setProduct = function(value,currency,idproduct){
+			$scope.value = value;
+  			$scope.currency = currency;
+  			$scope.idproduct = idproduct;
+
+
+		    }
+
+		   $scope.doCheckoutSimple = function(token) {
+	  		var email = token.email;
+		   	var token = token.id;
+		   	var value = $scope.value;
+		   	var currency = $scope.currency;
+		   	var idproduct = $scope.idproduct;
+
+		   		$http.post(CONFIG.url + '/payment/stripe/', {
+	                email: email,
+	                token: token,
+	                value: value,
+	                currency: currency,
+	                idproduct: idproduct
+	            }).success(function(data, status, headers, config) {
+	                
+
+	                console.log("DONE")
+	                console.log(data)
+
+	                /*
+	                var formDataArray = [];
+	                formDataArray["GoogleAccessId"] = data.GoogleAccessId;
+	                formDataArray["signature"] = data.signature;
+	                formDataArray["policy"] = data.policy;
+	                formDataArray["key"] = data.key;
+	                */
+
+	               
+
+	            }).error(function(data, status, headers, config) {
+	                // called asynchronously if an error occurs
+	                // or server returns response with an error status.
+	            });
+          };
+         
+
+  });
