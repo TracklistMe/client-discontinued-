e55ab0f37caff6e5330d8d1fc079c58b81889752
Nicola Bortignon <info@nicolabortignon.com>
2015-03-02 13:07:38 +0000

    https api endpoint

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ var app = angular.module('tracklistmeApp', [
     'angularFileUpload',
     'stripe.checkout'
 ]).constant("CONFIG", {
-    "url": "http://tracklist.me/api/",
+    "url": "https://tracklist.me/api/",
     "imagePath": "image"
 })
 
