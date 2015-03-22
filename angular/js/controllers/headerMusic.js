11e2334aca2c900b21665796bd154224eb8e7ff3
Nicola Bortignon <info@nicolabortignon.com>
2015-03-22 21:16:02 +0000

    attempt grunt

---
+++ angular/js/controllers/headerMusic.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* Controllers */
+  // signin controller
+app.controller('headerMusicController', function($scope, $auth, Account, CONFIG, $rootScope) {
+        $scope.serverURL = CONFIG.url
+        $scope.showAccountDropDown = false
+        
+        $scope.epxandCheckout = false;
+
+        console.log("@ASD")
+        $scope.isAuthenticated = function() {
+            return $auth.isAuthenticated();
+        };
+
+        Account.getProfile()
+            .success(function(data) {
+                console.log(data)
+                $scope.user = data;
+                console.log(data)
+                $scope.user.avatar = $scope.serverURL + "/images/" + $scope.user.avatar;
+            })
+
+        $scope.clickSearchButton = function() {
+            console.log("HIT")
+            $rootScope.$broadcast('searchActivate', 'test');
+        }
+
+    });
