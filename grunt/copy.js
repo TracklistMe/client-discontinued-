aee954720d929a75df0bbd3c14dffb6edd1cab2a
Nicola Bortignon <info@nicolabortignon.com>
2015-04-23 12:11:30 +0100

    add nestle in deploy

--- grunt/copy.js
+++ grunt/copy.js
@@ -53,6 +53,11 @@ module.exports = {
         }, {
             expand: true,
             src: "**",
+            cwd: 'bower_components/nestable/',
+            dest: "angular/bower_components/nestable"
+        }, {
+            expand: true,
+            src: "**",
             cwd: 'bower_components/html5sortable/',
             dest: "angular/bower_components/htmlSortable"
         }, {
