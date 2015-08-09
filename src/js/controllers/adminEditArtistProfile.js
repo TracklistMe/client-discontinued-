89f674e9e7fbf8401438f44fae68ea72fdba11f8
Nicola Bortignon <info@nicolabortignon.com>
2015-08-09 10:12:28 -0700

    Start linting

--- src/js/controllers/adminEditArtistProfile.js
+++ src/js/controllers/adminEditArtistProfile.js
@@ -7,41 +7,40 @@
  * # AdmincompaniesCtrl
  * Controller of the tracklistmeApp
  */
-app.controller('AdminEditArtistProfile', function($location, $scope, $state, $auth, $stateParams, $http, $modal, Account, FileUploader, CONFIG) {
-    var artistId = $scope.artistId = $stateParams.id
-    $scope.serverURL = CONFIG.url
+app.controller('AdminEditArtistProfile', 
+    function($location, $scope, $state, $auth, $stateParams, $http, $modal, 
+        Account, FileUploader, CONFIG) {
+    var artistId = $scope.artistId = $stateParams.id;
+    $scope.serverURL = CONFIG.url;
 
     $scope.itemsByPage = 10;
     $scope.currentArtist = null;
-    $scope.isSearching = false
-    $scope.editIsSearching = false
-    $scope.nameAvailable = false
-    $scope.editNameAvailable = false
+    $scope.isSearching = false;
+    $scope.editIsSearching = false;
+    $scope.nameAvailable = false;
+    $scope.editNameAvailable = false;
 
-    $scope.nameTooShort = true
-    $scope.editNameTooShort = true
-    $scope.searchUserResults = []
-    console.log("CONTROLELR ADMINARTIST")
-    $scope.artist;
-    // should be merged with the underneath function searchForEditArtistNameAvailability
-
+    $scope.nameTooShort = true;
+    $scope.editNameTooShort = true;
+    $scope.searchUserResults = [];
+   
+    var artistEndPoint = CONFIG.url + '/artists/';
+    var profilePictureAPIEndPoint = artistEndPoint + $scope.artistId 
+    + '/profilePicture/1400/1400/';
 
     var uploader = $scope.uploader = new FileUploader({
-        url: CONFIG.url + '/artists/' + $scope.artistId + '/profilePicture/1400/1400/',
+        url: profilePictureAPIEndPoint,
         headers: {
             'Authorization': 'Bearer ' + $auth.getToken()
         },
         data: {
             user: $scope.user
-        },
+        }
     });
     uploader.onProgressItem = function(fileItem, progress) {
-        console.info('onProgressItem', fileItem, progress);
         $scope.loadedImage = progress;
-
     };
     uploader.onAfterAddingFile = function(fileItem) {
-        console.info('onAfterAddingFile', fileItem);
         uploader.queue[0].upload();
         uploader.queue.pop();
     };
