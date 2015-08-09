e01d44065037976a6821defe32e5d7ebb3aa6a89
Nicola Bortignon <info@nicolabortignon.com>
2015-08-09 12:45:37 -0700

    Added back customized jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,31 +5,34 @@ module.exports = function(grunt) {
 
   var gruntConfig = require('./grunt');
   gruntConfig.package = require('./package.json');
-  gtx.config(gruntConfig);
 
-  // We need our bower components in order to develop
-  gtx.alias('build:angular', ['recess:less', 'clean:angular', 'copy:angular', 'recess:angular', 'concat:angular', 'uglify:angular']);
-  gtx.alias('build:html', ['clean:html', 'copy:html', 'recess:html', 'swig:html', 'concat:html', 'uglify:html']);
-  gtx.alias('build:landing', ['copy:landing', 'swig:landing']);
-  gtx.alias('lint',['jshint']);
-  gtx.alias('release', ['bower-install-simple', 'build:angular', 'bump-commit']);
-  gtx.alias('release-patch', ['bump-only:patch', 'release']);
-  gtx.alias('release-minor', ['bump-only:minor', 'release']);
-  gtx.alias('release-major', ['bump-only:major', 'release']);
-  gtx.alias('prerelease', ['bump-only:prerelease', 'release']);
-
- 
- 
-
-  grunt.initConfig({
+  //Configure modules that required specific setup
+  gtx.config({
     jshint: {
       options: {
         jshintrc: '.jshintrc',
         reporter: require('jshint-stylish')
       }
     }
-  })
-      
-   
+  });
+
+  gtx.config(gruntConfig);
+  //Built the client 
+  gtx.alias('build:angular', ['recess:less', 'clean:angular', 'copy:angular', 'recess:angular', 'concat:angular', 'uglify:angular']);
+  //Build the html miniwebsite 
+  gtx.alias('build:html', ['clean:html', 'copy:html', 'recess:html', 'swig:html', 'concat:html', 'uglify:html']);
+  //Build the landing page
+  gtx.alias('build:landing', ['copy:landing', 'swig:landing']);
+  //Run the linter
+  gtx.alias('lint', ['jshint']);
+
+  /* Do not use the following command, they are not consistent at the momet.
+  gtx.alias('release', ['bower-install-simple', 'build:angular', 'bump-commit']);
+  gtx.alias('release-patch', ['bump-only:patch', 'release']);
+  gtx.alias('release-minor', ['bump-only:minor', 'release']);
+  gtx.alias('release-major', ['bump-only:major', 'release']);
+  gtx.alias('prerelease', ['bump-only:prerelease', 'release']);
+  */
+
   gtx.finalise();
 }
