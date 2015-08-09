d2a6fefca141dd31da1e53c05a8277924a83de87
Nicola Bortignon <info@nicolabortignon.com>
2015-08-09 14:44:07 -0700

    Listed main.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: ['**/*.js'],
-        tasks: ['lint --force']
+        tasks: ['lint']
       },
     },
   });
@@ -66,7 +66,7 @@ module.exports = function(grunt) {
   gtx.alias('lint', ['jshint']);
   //Run the webserver on the src/ folder and keep watching the changes.
   //Every time you save a file the linter starts.
-  gtx.alias('develop', ['lint','concurrent:developEnviroment']);
+  gtx.alias('develop', ['concurrent:developEnviroment']);
   //Start the local webserver with the debug version of the site
   gtx.alias('serve:debug', ['connect:debug:keepalive']);
   //Compile the site and start the local webserver on the production code
--- src/js/main.js
+++ src/js/main.js
@@ -1,93 +1,100 @@
+/*jslint browser:true */
 'use strict';
 
 /* Controllers */
 
 angular.module('app')
-    .controller('AppCtrl', ['$scope', '$translate', '$localStorage', '$window',
-        function($scope, $translate, $localStorage, $window) {
-            // add 'ie' classes to html
-            var isIE = !!navigator.userAgent.match(/MSIE/i);
-            isIE && angular.element($window.document.body).addClass('ie');
-            isSmartDevice($window) && angular.element($window.document.body).addClass('smart');
-
-            // config
-            $scope.app = {
-                name: 'Tracklist.me',
-                version: '0.0.4',
-                // for chart colors
-                color: {
-                    primary: '#7266ba',
-                    info: '#23b7e5',
-                    success: '#27c24c',
-                    warning: '#fad733',
-                    danger: '#f05050',
-                    light: '#e8eff0',
-                    dark: '#3a3f51',
-                    black: '#1c2b36'
-                },
-                settings: {
-                    themeID: 7,
-                    headerFixed: true,
-                    asideFixed: false,
-                    asideFolded: true,
-                    asideDock: true,
-                    container: true
-                }
-            }
-
-
-            $scope.app.settings.themeID = 1;
-            $scope.app.settings.asideFolded = true;
-            $scope.app.settings.asideDock = false;
-            $scope.app.settings.asideFixed = true;
-            $scope.app.settings.container = true;
-            $scope.app.settings.navbarCollapseColor = "bg-white-only"
-            $scope.app.settings.navbarHeaderColor = "bg-black"
-            $scope.app.settings.asideColor = "bg-black"
-
-
-            console.log($scope.app.settings)
-            // save settings to local storage
-            /*
-            if (angular.isDefined($localStorage.settings)) {
-                $scope.app.settings = $localStorage.settings;
-            } else {
-                $localStorage.settings = $scope.app.settings;
-            }
-            */
-            $scope.$watch('app.settings', function() {
-                if ($scope.app.settings.asideDock && $scope.app.settings.asideFixed) {
-                    // aside dock and fixed must set the header fixed.
-                    $scope.app.settings.headerFixed = true;
-                }
-                // save to local storage
-                $localStorage.settings = $scope.app.settings;
-            }, true);
-
-            // angular translate
-            $scope.lang = {
-                isopen: false
-            };
-            $scope.langs = {
-                en: 'English',
-                de_DE: 'German',
-                it_IT: 'Italian'
-            };
-            $scope.selectLang = $scope.langs[$translate.proposedLanguage()] || "English";
-            $scope.setLang = function(langKey, $event) {
-                // set the current lang
-                $scope.selectLang = $scope.langs[langKey];
-                // You can change the language during runtime
-                $translate.use(langKey);
-                $scope.lang.isopen = !$scope.lang.isopen;
-            };
-
-            function isSmartDevice($window) {
-                // Adapted from http://www.detectmobilebrowsers.com
-                var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
-                // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
-                return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
-            }
-
+  .controller('AppCtrl', ['$scope', '$translate', '$localStorage', '$window',
+    function($scope, $translate, $localStorage, $window) {
+      // add 'ie' classes to html
+      var isIE = navigator.userAgent.match(/MSIE/i);
+      if (isIE) {
+        angular.element($window.document.body).addClass('ie');
+      }
+      if (isSmartDevice($window)) {
+        angular.element($window.document.body).addClass('smart');
+      }
+      // config
+      $scope.app = {
+        name: 'Tracklist.me',
+        version: '0.0.4',
+        // for chart colors
+        color: {
+          primary: '#7266ba',
+          info: '#23b7e5',
+          success: '#27c24c',
+          warning: '#fad733',
+          danger: '#f05050',
+          light: '#e8eff0',
+          dark: '#3a3f51',
+          black: '#1c2b36'
+        },
+        settings: {
+          themeID: 7,
+          headerFixed: true,
+          asideFixed: false,
+          asideFolded: true,
+          asideDock: true,
+          container: true
         }
-    ]);
+      };
+
+
+      $scope.app.settings.themeID = 1;
+      $scope.app.settings.asideFolded = true;
+      $scope.app.settings.asideDock = false;
+      $scope.app.settings.asideFixed = true;
+      $scope.app.settings.container = true;
+      $scope.app.settings.navbarCollapseColor = 'bg-white-only';
+      $scope.app.settings.navbarHeaderColor = 'bg-black';
+      $scope.app.settings.asideColor = 'bg-black';
+
+      // save settings to local storage
+      /*
+      if (angular.isDefined($localStorage.settings)) {
+          $scope.app.settings = $localStorage.settings;
+      } else {
+          $localStorage.settings = $scope.app.settings;
+      }
+      */
+      $scope.$watch('app.settings', function() {
+        if ($scope.app.settings.asideDock && $scope.app.settings.asideFixed) {
+          // aside dock and fixed must set the header fixed.
+          $scope.app.settings.headerFixed = true;
+        }
+        // save to local storage
+        $localStorage.settings = $scope.app.settings;
+      }, true);
+
+      // angular translate
+      $scope.lang = {
+        isopen: false
+      };
+      $scope.langs = {
+        en: 'English',
+        de_DE: 'German',
+        it_IT: 'Italian'
+      };
+      $scope.selectLang = $scope.langs[$translate.proposedLanguage()] ||
+        $scope.langs.en;
+      $scope.setLang = function(langKey) {
+        // set the current lang
+        $scope.selectLang = $scope.langs[langKey];
+        // You can change the language during runtime
+        $translate.use(langKey);
+        $scope.lang.isopen = !$scope.lang.isopen;
+      };
+
+      function isSmartDevice($window) {
+        // Adapted from http://www.detectmobilebrowsers.com
+        var ua = $window.navigator.userAgent ||
+          $window.navigator.vendor ||
+          $window.opera;
+        // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile 
+        // devices
+        return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).
+        test(ua);
+      }
+
+    }
+  ]);
