8691ce592fa8ded7c8eae64eb0c789dd95ee106a
Nicola Bortignon <info@nicolabortignon.com>
2015-08-27 14:41:00 -0700

    adding data picker for revenue in company page.

--- src/js/controllers/adminLabel.js
+++ src/js/controllers/adminLabel.js
@@ -212,6 +212,66 @@ app.controller('AdminlabelCtrl', function($location, $scope, $state, $auth,
       });
   };
 
+
+  // REVENUE PART //
+
+  // DATA PICKER 
+
+  var CHARACTER_BEFORE_SEARCH = 1;
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+  };
+
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+  };
+
+
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    if (!$scope.opened || $scope.opened === false) {
+      $scope.opened = true;
+    } else {
+      $scope.opened = false;
+    }
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1,
+    class: 'datepicker'
+  };
+
+  $scope.initDate = new Date();
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  //END OF DATA PICKER 
+
+
+
+
   $scope.getToProcessReleases();
   $scope.getDropZoneFiles();
   $scope.getCatalog();
